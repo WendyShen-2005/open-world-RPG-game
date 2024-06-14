b497e8270c586ccf4b360cff283a91d5477adfcf
Wendy <wendyshen2005@gmail.com>
2024-06-13 22:19:20 -0400

    made camera follow player with this tutorial: https://www.youtube.com/watch?v=u7LPRqrzry8&t=1656s

---
+++ client/environment/tree.py
@@ -0,0 +1,10 @@
+import os.path
+
+import pygame
+
+
+class Tree(pygame.sprite.Sprite):
+    def __init__(self, pos, group):
+        super().__init__(group)
+        self.image = pygame.image.load(os.path.join('client', 'environment', 'tree.png')).convert_alpha()
+        self.rect = self.image.get_rect(topleft = pos)
